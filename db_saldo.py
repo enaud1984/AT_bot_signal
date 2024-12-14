69aaf753b39734261150bf65b5da9f43b1dc6d98
enaud1984 <caesar_539@hotmail.it>
2024-12-14 23:38:46 +0100

    Fix db_saldo.py
    Co  sqllote nn posso usare with cursor

--- db_saldo.py
+++ db_saldo.py
@@ -6,8 +6,8 @@ class SaldoDB:
         self.db_name=db_name
         # Connessione al database
         with sqlite3.connect(db_name) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("""
+            cursor =conn.cursor()
+            cursor.execute("""
                     CREATE TABLE IF NOT EXISTS saldo (
                         symbol TEXT PRIMARY KEY,
                         amount REAL
@@ -17,21 +17,21 @@ class SaldoDB:
     def initialize_saldo(self, symbol_list, saldo_totale):
         for symbol in symbol_list:
             with sqlite3.connect(self.db_name) as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute("SELECT amount FROM saldo WHERE symbol = ?", (symbol,))
-                    result = cursor.fetchone()
-                    if result is None:  # Se il simbolo non è nella tabella, lo inizializza
-                        saldo_iniziale = saldo_totale / len(symbol_list)
-                        cursor.execute("INSERT INTO saldo (symbol, amount) VALUES (?, ?)", (symbol, saldo_iniziale))
+                cursor = conn.cursor()
+                cursor.execute("SELECT amount FROM saldo WHERE symbol = ?", (symbol,))
+                result = cursor.fetchone()
+                if result is None:  # Se il simbolo non è nella tabella, lo inizializza
+                    saldo_iniziale = saldo_totale / len(symbol_list)
+                    cursor.execute("INSERT INTO saldo (symbol, amount) VALUES (?, ?)", (symbol, saldo_iniziale))
 
     def get_saldo(self, symbol):
         with sqlite3.connect(self.db_name) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT amount FROM saldo WHERE symbol = ?", (symbol,))
-                result = cursor.fetchone()
-                return result[0] if result else 0
+            cursor = conn.cursor() as cursor:
+            cursor.execute("SELECT amount FROM saldo WHERE symbol = ?", (symbol,))
+            result = cursor.fetchone()
+        return result[0] if result else 0
 
     def set_saldo(self, symbol, nuovo_saldo):
         with sqlite3.connect(self.db_name) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("UPDATE saldo SET amount = ? WHERE symbol = ?", (nuovo_saldo, symbol))
+            cursor =conn.cursor()
+             cursor.execute("UPDATE saldo SET amount = ? WHERE symbol = ?", (nuovo_saldo, symbol))
